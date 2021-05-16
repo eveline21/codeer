69d27ab7466d5af8af974c5cbf42850846da94b1
Eveline Coene <eveline.coene@student.kdg.be>
2021-05-16 16:38:17 +0200

    navigate to home

--- frontend/src/components/page.tsx
+++ frontend/src/components/page.tsx
@@ -2,6 +2,7 @@ import { Layout } from "antd";
 import { Content, Header } from "antd/es/layout/layout";
 import React, { FC, ReactNode } from "react";
 import logo from "../logo.png";
+import { Link } from "react-router-dom";
 
 interface Props {
     children: ReactNode;
@@ -11,7 +12,7 @@ export const Page: FC<Props> = ({ children }) => {
     return (
         <Layout>
             <Header>
-                <img src={logo} style={{ width: '150px' }} className="App-logo" alt="logo" />
+                <Link to="/"><img src={logo} style={{ width: '150px' }} className="App-logo" alt="logo" /></Link>
             </Header>
             <Content style={{ height: "100%" }}>{children}</Content>
         </Layout>
